a24493c9cdfb2b998dd98d71ab977c92f570726d
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2022-06-20 11:15:43 +0200

    code review PR#3

--- demo.c
+++ demo.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 
+
+int addone(int x);
+int carre(int x);
+
 int main() {
 	printf("hello\n");
-	printf("carre = %d\n", addone(5));
+	printf("carre = %d\n", carre(5));
 }
 
+// ajoute 1 à x
 int addone(int x) {
 	return x+1;
 }
 
-int carre(x) {
-	return x*x
-	;
+int carre(int x) {
+	return x*x;
 }
